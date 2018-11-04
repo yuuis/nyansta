70b63c0a8c6f78708bbfe7ee98572ccd8a5157d0
Yu Ishiguro <yuuis.egr@gmail.com>
2018-11-04 17:51:06 +0900

    verify routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,4 @@ Rails.application.routes.draw do
   resources :cats, only: [:show] do
     get :search, on: :collection
   end
-
-  # get "/cat/prefuscture/:prefecture" => "cat#search"
-  # get "/cat/cafe/:cafe_id" => "cat#search"
-  # get "/cat/breed/:breed_id" => "cat#search"
-  # get "cat/age/:age" => "cat#search"
-
-  get "/cat/:prefecture/:breed_id" => "cat#search_for_prefecture_and_breed"
-  
 end
